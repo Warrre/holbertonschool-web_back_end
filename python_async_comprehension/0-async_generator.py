a559e96b6098c95b636d05ddfa1fe335163a490b
warrre <warren.soulsey@gmail.com>
2025-08-01 23:55:59 +0200

    Refactor code by adding blank lines for improved readability in async generator and async comprehension modules

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,6 +5,7 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Yield a random float between 0 and 10, 10 times with 1s delay.
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,10 @@
 """This module provides a coroutine using async comprehension."""
 
 from typing import List
+
+
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """
