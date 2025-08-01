fb7223ca76f07c1a37f06aafa1d55acee4a9d65d
warrre <warren.soulsey@gmail.com>
2025-08-01 23:50:19 +0200

    Refactor code by adding blank lines for improved readability across multiple modules.

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -3,7 +3,10 @@
 
 import asyncio
 from typing import Any
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,7 @@
 
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Calculate the sum of a list containing integers and floats.
