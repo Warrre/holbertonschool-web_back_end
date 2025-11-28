52fd6efaf5476c976faf6ce94263654f6350f1bd
warrre <warren.soulsey@gmail.com>
2025-11-28 22:41:55 +0100

    Mise à jour de la documentation de la fonction async_generator pour clarifier son fonctionnement

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,8 +9,8 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    Loops 10 times, each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10.
+    The coroutine will loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10. Use the random module.
     """
     for _ in range(10):
         await asyncio.sleep(1)
