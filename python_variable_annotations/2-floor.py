cf4c880e6c095b602cf4c469fba8f4f8039f6015
warrre <warren.soulsey@gmail.com>
2025-11-28 22:20:34 +0100

    Ajout de fonctions annotées en Python et mise à jour des fichiers README

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float n as argument and returns the floor of the float.
+    """
+    return math.floor(n)
---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Annotate the below function’s parameters and return values
+with the appropriate types
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples with the element and its length
+    """
+    return [(i, len(i)) for i in lst]
