e3bfb541f812d38b6b60bf376e41cc96a2c302ad
warrre <warren.soulsey@gmail.com>
2025-12-05 23:26:05 +0100

    Refactor le script 12-log_stats.py pour améliorer la lisibilité : utilisation de f-strings et suppression des commentaires superflus.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-"""
-Log stats
-"""
+"""Log stats"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
+
     nginx_collection = client.logs.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print("{} logs".format(n_logs))
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        print(f"\tmethod {method}: {count}")
 
-    status_check = nginx_collection.count_documents(
+    status_check_count = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print("{} status check".format(status_check))
+    print(f"{status_check_count} status check")
