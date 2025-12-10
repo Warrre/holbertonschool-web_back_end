38a589b502faa05174ff69d5975b62a353fa0ccd
warrre <warren.soulsey@gmail.com>
2025-12-10 14:19:21 +0100

    Suppression de code redondant dans la fonction concatArrays et amélioration de la lisibilité

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,9 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-const array1 = ['a','b'];
-const array1 = ['a','b'];
-
-const combined = [...array1, ...array2];
-
-const str = "Hello";
-const char = [...str];
+  return [...array1, ...array2, ...string];
 }
