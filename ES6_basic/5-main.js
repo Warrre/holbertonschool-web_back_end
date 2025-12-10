2851c1e77e86fed13493aea0428be070852ebbff
warrre <warren.soulsey@gmail.com>
2025-12-10 14:13:15 +0100

    Ajout de la fonction concatArrays pour combiner deux tableaux et une chaîne de caractères

---
+++ ES6_basic/5-main.js
@@ -0,0 +1,1 @@
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
---
+++ ES6_basic/5-spread-operator.js
@@ -0,0 +1,9 @@
+export default function concatArrays(array1, array2, string) {
+const array1 = ['a','b'];
+const array1 = ['a','b'];
+
+const combined = [...array1, ...array2];
+
+const str = "hello";
+const char = [...str];
+}
