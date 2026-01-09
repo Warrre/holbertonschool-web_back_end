08ab5c58d3fb3b6e96b614080d6577ff6b3772ae
Warren <warre@example.com>
2026-01-09 02:02:04 +0100

    Add stdin.js to prompt user for their name and handle input

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (chunk) => {
+  const input = chunk.toString().trim();
+  if (input.length > 0) {
+    process.stdout.write(`Your name is: ${input}\n`);
+  } else {
+    process.stdout.write('Your name is: \n');
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
