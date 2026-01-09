bc06318ebde505f62f1aa0d0b830321aff48572d
Warren <warre@example.com>
2026-01-09 02:02:48 +0100

    Refactor string quotes in stdin.js for consistency

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding("utf8");
 
-process.stdin.on('data', (chunk) => {
+process.stdin.on("data", (chunk) => {
   const input = chunk.toString().trim();
   if (input.length > 0) {
     process.stdout.write(`Your name is: ${input}\n`);
   } else {
-    process.stdout.write('Your name is: \n');
+    process.stdout.write("Your name is: \n");
   }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
 });
